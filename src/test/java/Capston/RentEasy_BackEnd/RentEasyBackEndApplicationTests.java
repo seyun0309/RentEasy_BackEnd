c1b724f93d18447e53b519e9117b3c7d8ecbd128
seyun kim <>
2024-06-20 18:26:00 +0900

    프로젝트 초기세팅

---
+++ src/test/java/Capston/RentEasy_BackEnd/RentEasyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package Capston.RentEasy_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentEasyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
