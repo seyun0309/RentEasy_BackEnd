0097c760aab3c64ff16c54b9b2a9337f4fad1d76
seyun kim <>
2024-06-24 16:33:12 +0900

    refactor : 비즈니스 예외 처리를 위해 ErrorCode 처리 추가

---
+++ src/main/java/Capston/RentEasy_BackEnd/global/error/exception/BusinessException.java
@@ -0,0 +1,20 @@
+package Capston.RentEasy_BackEnd.global.error.exception;
+
+
+import Capston.RentEasy_BackEnd.global.enums.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
